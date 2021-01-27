6700f9493a83d20326f70368f427a2d1a83e5d47
Emerson <veraraul495@gmail.com>
2021-01-27 16:11:41 -0500

    Corrrecion Pages

--- Examen/src/app/app-routing.module.ts
+++ Examen/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
 
 
 const routes: Routes = [
-  { path: '' , redirectTo: '/home' , pathMatch: 'full'},
- 
+  { path: '' , redirectTo: '/dashboard' , pathMatch: 'full'},
+  { path: '**', component: PagesComponent }
+
 ]
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
